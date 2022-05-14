372a0eb379f9181492c0d4f36190879c7b9062d7
thiiagoms <thiagom.devsec@gmail.com>
2022-05-14 12:51:37 -0300

    feat: create book routes, model and controller :sparkles:

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 ## Install
 
-- First, clone the project:
+-   First, clone the project:
 
 ```bash
 $ git clone https://github.com/thiiagoms/node-express-mongodb
 ```
 
-- Second, install dependencies:
+-   Second, install dependencies:
 
 ```bash
 $ cd node-express-mongodb
@@ -17,8 +17,16 @@ $ npm install
 
 ## Run
 
-- Run dev mode with `nodemon`:
+-   Run dev mode with `nodemon`:
 
 ```
 $ npm run dev
 ```
+
+## Endpoints
+
+-   `GET - /books` - Return all books
+-   `GET - /books/:id` - Return single book
+-   `POST - /books` - Create new book
+-   `PUT - /books/:id` - Update book
+-   `DELETE - /books/:id` - Delete book
--- src/app.mjs
+++ src/app.mjs
@@ -1,5 +1,6 @@
 import express from "express";
 import db from "./config/database.mjs";
+import router from "./routes/routes.js";
 
 db.on("error", console.log.bind(console, "Database connection error"));
 db.once("open", () => console.log("[*] Database connected!!"));
@@ -8,44 +9,6 @@ const app = express();
 
 app.use(express.json());
 
-const books = [
-  { id: 1, title: "Game of thrones" },
-  { id: 2, title: "The Chronicles of Narnia" },
-];
-
-app.get("/", (_, response) => {
-  response.status(200).send("Node course");
-});
-
-app.get("/books", (_, response) => {
-  response.status(200).json(books);
-});
-
-app.get("/books/:id", (request, response) => {
-  let index = searchBooks(request.params.id);
-  response.status(200).json(books[index]);
-});
-
-app.post("/books", (request, response) => {
-  books.push(request.body);
-  response.status(201).send("Pushed success");
-});
-
-app.put("/books/:id", (request, response) => {
-  let index = searchBooks(request.params.id);
-  books[index].title = request.body.title;
-  response.status(200).json(books);
-});
-
-app.delete("/books/:id", (request, response) => {
-  let { id } = request.params;
-  let index = searchBooks(id);
-  books.splice(index, 1);
-  response.status(200).send(`Book ${id} removed`);
-});
-
-function searchBooks(id) {
-  return books.findIndex((book) => book.id === parseInt(id));
-}
+router(app);
 
 export default app;
